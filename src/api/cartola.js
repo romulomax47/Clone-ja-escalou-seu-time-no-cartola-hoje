ea6f14aee4e3afa9d9348953502e0881e7715b82
Romulo Max <maxromulo16@gmail.com>
2022-07-05 16:38:58 -0300

    add moment

--- src/api/cartola.js
+++ src/api/cartola.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const BASE_URL = process.env.CARTOLA_API
 
 async function fetchMarketStatus(){
-    const data = await axios.get(`${BASE_URL}`).then(({data}) => data)
+    const data = await axios.get(`https://api.cartola.globo.com/mercado/status`).then(({data}) => data)
                             .catch((error) => error)
     
          if(data instanceof Error) {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const dotenv = require("dotenv")
 dotenv.config()
+const moment = require("moment")
 const {fetchMarketStatus, BASE_URL } = require('./api/cartola')
 // const axios = require('axios');
 
 
 async function main (){
-    const fetchMarket = await  fetchMarketStatus();
-    return console.log(fetchMarket)
+    const fetchMarket = await  fetchMarketStatus(); 
+    const dataFormaTBr = moment().format('DD/MM/YYYY HH:mm')
+    console.log(dataFormaTBr)
+        return console.log(fetchMarket)
 }
 
 main()
